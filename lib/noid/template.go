45a4711b1ee10087b727a89395c1e92fb051a8bc
Nerdmaster <brody76@gmail.com>
2014-04-09 20:07:30 -0700

    Add template type, logic, and test

---
+++ lib/noid/template.go
@@ -0,0 +1,66 @@
+package noid
+
+import (
+	"strings"
+	"errors"
+	"fmt"
+)
+
+type Ordering int
+
+const (
+	Random Ordering = iota
+	SequentialLimited
+	SequentialUnlimited
+)
+
+type Template struct {
+	prefix string
+	ordering Ordering
+	mask string
+	hasCheckDigit bool
+}
+
+// 14-character masks can theoretically result in a string that can't be
+// represented by an int64 index, and for now I want all noids to be easily
+// converted to and from a raw number.  13 characters, if all are using the
+// "extended" set, is still enough to hold over 10 quintillion (10 billion
+// billion) noids.
+const MaxMaskLength = 13
+
+func NewTemplate(template string) (*Template, error) {
+	var suffix string
+
+	// You know what's hip and cool these days?  Storing values immediately on
+	// instantiation when said values are essentially static, read-only data
+	t := &Template{}
+	parts := strings.Split(template, ".")
+
+	if len(parts) == 1 {
+		t.prefix = ""
+		suffix = parts[0]
+	} else {
+		t.prefix = parts[0]
+		suffix = parts[1]
+	}
+
+	last := len(suffix) - 1
+	if suffix[last] == 'k' {
+		t.hasCheckDigit = true
+		suffix = suffix[0:last]
+	}
+
+	switch suffix[0] {
+		case 'r': t.ordering = Random
+		case 's': t.ordering = SequentialLimited
+		case 'z': t.ordering = SequentialUnlimited
+	}
+
+	t.mask = suffix[1:]
+
+	if len(t.mask) > MaxMaskLength {
+		return nil, errors.New(fmt.Sprintf("Mask cannot be more than %d characters", MaxMaskLength))
+	}
+
+	return t, nil
+}
---
+++ lib/noid/template_test.go
@@ -0,0 +1,53 @@
+package noid
+
+import "testing"
+
+func assertTemplateAttributeS(templateString, attribute, expected, actual string, t *testing.T) {
+	if (expected != actual) {
+		t.Errorf("Expected %#v to have %s %#v, but got %#v", templateString, attribute, expected, actual)
+	}
+}
+
+func assertTemplateAttributeO(templateString string, attribute string, expected Ordering, actual Ordering, t *testing.T) {
+	if (expected != actual) {
+		t.Errorf("Expected %#v to have %s %#v, but got %#v", templateString, attribute, expected, actual)
+	}
+}
+
+func assertTemplateAttributeB(templateString string, attribute string, expected bool, actual bool, t *testing.T) {
+	if (expected != actual) {
+		t.Errorf("Expected %#v to have %s %#v, but got %#v", templateString, attribute, expected, actual)
+	}
+}
+
+func TestTemplatesWithPrefix(t *testing.T) {
+	str := "prefix.reedeek"
+	template, _ := NewTemplate(str)
+
+	assertTemplateAttributeS(str, "prefix", "prefix", template.prefix, t)
+	assertTemplateAttributeS(str, "mask", "eedee", template.mask, t)
+	assertTemplateAttributeO(str, "ordering", Random, template.ordering, t)
+	assertTemplateAttributeB(str, "hasCheckDigit", true, template.hasCheckDigit, t)
+}
+
+func TestTemplatesWithoutPrefix(t *testing.T) {
+	str := "reedeek"
+	template, _ := NewTemplate(str)
+
+	assertTemplateAttributeS(str, "prefix", "", template.prefix, t)
+	assertTemplateAttributeS(str, "mask", "eedee", template.mask, t)
+	assertTemplateAttributeO(str, "ordering", Random, template.ordering, t)
+	assertTemplateAttributeB(str, "hasCheckDigit", true, template.hasCheckDigit, t)
+}
+
+func TestLongTemplates(t *testing.T) {
+	str := "prefix.reedeedeedeedeedeedeedeedeek"
+	template, err := NewTemplate(str)
+	if template != nil {
+		t.Errorf("Expected %#v to be invalid, but template was non-nil", str)
+	}
+
+	if err == nil {
+		t.Errorf("Expected %#v to be invalid, but err was nil", str)
+	}
+}
