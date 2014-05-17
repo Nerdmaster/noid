c1e2eed5044cc0ad101b126ca2f8c4d30bc56802
Nerdmaster <brody76@gmail.com>
2014-05-16 20:10:20 -0700

    Add bitSwap code
    For "random" noids of an exact bit size, this allows us to swap bits in order
    to fake the appearance of a random sequence

---
+++ lib/noid/bitswap.go
@@ -0,0 +1,8 @@
+package noid
+
+func bitSwap(val uint64, b1 byte, b2 byte) uint64 {
+	if (val & uint64(1 << b1)) ^ ((val & uint64(1 << b2)) >> (b2 - b1)) != 0 {
+		return (val ^ uint64(1 << b1)) ^ (1 << b2)
+	}
+	return val
+}
